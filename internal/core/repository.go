c348491cdf83446c9789c3592a1ee1e638544717
Lukas Grimm <lukaskingsmail@gmail.com>
2023-07-08 11:20:50 +0200

    Implement v1 repo with gorm

--- internal/core/repository.go
+++ internal/core/repository.go
@@ -1,16 +1,70 @@
 package core
 
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
 // A repository persists entities of a given type.
 // Entities must be unique by their id.
 type Repository[IdType comparable, Type interface{}] interface {
-	FindAll() ([]Type, error)
-	FindById(id IdType) (Type, error)
-	Save(entity Type) error
-	Update(entity Type) error
-	DeleteById(id IdType) error
+	FindAll(ctx context.Context) ([]Type, error)
+	FindById(ctx context.Context, id IdType) (Type, error)
+	Save(ctx context.Context, entity Type) error
+	Update(ctx context.Context, entity Type) error
+	DeleteById(ctx context.Context, id IdType) error
 }
 
 type TenancyRepository[IdType comparable, Type interface{}] interface {
 	Repository[IdType, Type]
-	FindAllByTenantId(tenantId string) ([]Type, error)
+	FindAllByTenantId(ctx context.Context, tenantId string) ([]Type, error)
+}
+
+type GormTenancyRepository[IdType comparable, Type interface{}] struct {
+	DB *gorm.DB
+}
+
+func NewGormRepository[IdType comparable, Type interface{}](db *gorm.DB) *GormTenancyRepository[IdType, Type] {
+	return &GormTenancyRepository[IdType, Type]{
+		DB: db,
+	}
+}
+
+func (repo *GormTenancyRepository[IdType, Type]) FindAll(ctx context.Context) ([]Type, error) {
+	var entities []Type
+	err := repo.DB.WithContext(ctx).Find(&entities).Error
+	return entities, err
+}
+
+func (repo *GormTenancyRepository[IdType, Type]) FindById(ctx context.Context, id IdType) (Type, error) {
+	var entity Type
+	err := repo.DB.WithContext(ctx).First(&entity, id).Error
+	return entity, err
+}
+
+func (repo *GormTenancyRepository[IdType, Type]) FindAllByTenantId(ctx context.Context, tenantId string) ([]Type, error) {
+	var entities []Type
+	err := repo.DB.WithContext(ctx).Where("tenant_id = ?", tenantId).Find(&entities).Error
+	return entities, err
+}
+
+func (repo *GormTenancyRepository[IdType, Type]) Save(ctx context.Context, entity Type) error {
+	err := repo.DB.WithContext(ctx).Create(&entity).Error
+	return err
+}
+
+func (repo *GormTenancyRepository[IdType, Type]) Update(ctx context.Context, entity Type) error {
+	err := repo.DB.WithContext(ctx).Save(&entity).Error
+	return err
+}
+
+func (repo *GormTenancyRepository[IdType, Type]) DeleteById(ctx context.Context, id IdType) error {
+	db := repo.DB.WithContext(ctx)
+	entity, err := repo.FindById(ctx, id)
+	if err != nil {
+		return err
+	}
+	err = db.Delete(&entity).Error
+	return err
 }
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/Untanky/modern-auth/internal/core"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Person struct {
@@ -15,6 +18,12 @@ func (p Person) String() string {
 }
 
 func main() {
+	runInMemoryKeyValueStore()
+	runGormRepository()
+}
+
+func runInMemoryKeyValueStore() {
+	log.Println("In Memory Key Value Store Example")
 	store := core.NewInMemoryKeyValueStore[Person]()
 
 	err := store.Set("1", &Person{Name: "John"})
@@ -31,4 +40,43 @@ func main() {
 
 	err = store.Delete("1")
 	log.Println("Delete:", err)
+}
+
+type Product struct {
+	gorm.Model
+	ID    string
+	Code  string
+	Price uint
+}
+
+func runGormRepository() {
+	log.Println("Gorm Repository Example")
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	repository := core.NewGormRepository[string, Product](db)
+
+	err = repository.Save(context.TODO(), Product{ID: "1", Code: "D42", Price: 100})
+	log.Println("Save:", err)
+
+	list, err := repository.FindAll(context.TODO())
+	log.Println("Find:", list, err)
+
+	value, err := repository.FindById(context.TODO(), "1")
+	log.Println("FindById:", value, err)
+
+	value.Price = 800
+	err = repository.Update(context.TODO(), value)
+	log.Println("Update:", err)
+
+	err = repository.DeleteById(context.TODO(), "1")
+	log.Println("Delete:", err)
+
+	list, err = repository.FindAll(context.TODO())
+	log.Println("Find:", list, err)
 }
