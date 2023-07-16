59b320db9d826761753899a34ae281c5ad8c7616
Lukas Grimm <lukaskingsmail@gmail.com>
2023-07-16 20:46:49 +0200

    Implement user login screen

--- client-app/src/App.svelte
+++ client-app/src/App.svelte
@@ -1,47 +1,24 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div class="flex w-screen h-screen justify-center items-center">
+  <main class="w-[420px] card">
+    <h1 class="text-stone-950 dark:text-stone-50 text-xl font-medium">
+      Login
+    </h1>
+    <p class="mt-2">
+      Please submit your user id.
+    </p>
+    <form class="flex flex-col flex-1">
+      <div class="relative mt-2">
+        <label class="dark:bg-stone-800 px-1 left-3 absolute block" for="user-id">
+          User Id:
+        </label>
+        <input class="dark:bg-stone-800 mt-3.5 px-4 py-2 dark:border-stone-600 border rounded-lg w-full" type="text" id="user-id" placeholder="Your user id" />
+      </div>
+      <button class="self-end mt-2 btn btn-primary">
+        Submit
+      </button>
+    </form>
+  </main>
+</div>
