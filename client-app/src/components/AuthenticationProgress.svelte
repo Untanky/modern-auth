ddc838816c43289b151115f3e0004dfd6f2e488b
Lukas Grimm <lukaskingsmail@gmail.com>
2023-08-03 17:43:47 +0200

    Make form a bit more reactive

--- client-app/src/components/AuthenticationProgress.svelte
+++ client-app/src/components/AuthenticationProgress.svelte
@@ -3,25 +3,15 @@
   import IdentificationIcon from './icons/Identification.svelte';
   import PersonIcon from './icons/Person.svelte';
 
-  export let progress = 0;
-
-  const gradient = `bg-gradient-to-r from-green-400 via-yellow-400 to-stone-50`;
-  const fullStone = 'bg-stone-50';
-  const halfYellow = `${gradient} from-0% via-0% to-50%`;
-  const fullGreen = 'bg-green-400';
-
-  $: first = progress === 0 ? halfYellow : fullGreen;
-  $: second = progress < 0.33333 ? fullStone : progress > 0.33333 ? fullGreen : halfYellow;
-  $: third = progress < 0.66667 ? fullStone : progress > 0.66667 ? fullGreen : halfYellow;
 </script>
 
 <div class="relative py-2">
   <div class="flex justify-between">
-    <div class={`p-px w-[58px] h-[58px] rounded-full ${first}`}/>
-    <div class={`p-px w-[58px] h-[58px] rounded-full ${second}`}/>
-    <div class={`p-px w-[58px] h-[58px] rounded-full ${third}`}/>
+    <div class="p-px w-[58px] h-[58px] rounded-full bg-stone-50"/>
+    <div class="p-px w-[58px] h-[58px] rounded-full bg-stone-50"/>
+    <div class="p-px w-[58px] h-[58px] rounded-full bg-stone-50"/>
   </div>
-  <div class={`absolute top-[34px] left-4 right-4 p-px progressive-bg ${gradient} from-0% via-[${progress * 100}%] to-[${progress * 100}%]`}>
+  <div class="absolute top-[34px] left-4 right-4 p-px bg-stone-50">
     <div class="h-1.5 dark:bg-stone-800"></div>
   </div>
   <div class="absolute left-0 right-0 top-2 flex justify-between">
--- client-app/src/components/forms/Identification.svelte
+++ client-app/src/components/forms/Identification.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   export let submit: (userId: string) => void;
 
-  const onClick = (event: Event) => {
+  let userId = '';
+
+  $: canSubmit = userId.length > 0;
+
+  const onClick = (event: SubmitEvent) => {
     event.preventDefault();
 
-    const inputElement = document.getElementById('user-id') as HTMLInputElement;
-
-    submit(inputElement.value);
+    submit(event.target[0].value);
   }
 </script>
 
-<form class="flex flex-col flex-1">
+<form class="flex flex-col flex-1" on:submit={onClick}>
   <h2 class="text-xl">
     Identification
   </h2>
@@ -18,12 +20,18 @@
     To authenticate, we need to know who you are. Please enter your user id.
   </p>
   <div class="relative mt-4">
-    <label class="font-extralight text-sm uppercase dark:bg-stone-800 px-1 left-3 absolute block" for="user-id">
-      User Id:
+    <label class="font-extralight text-sm uppercase dark:bg-stone-800 px-1 left-3 absolute block rounded" for="user-id">
+      User ID:
     </label>
-    <input class="dark:bg-stone-800 mt-3 px-4 py-2 dark:border-stone-600 border rounded-lg w-full" type="text" id="user-id" placeholder="Your user id" />
+    <input
+      bind:value={userId}
+      class="dark:bg-stone-800 mt-3 px-4 py-2 dark:border-stone-600 border rounded-lg w-full focus:outline-none focus:border-yellow-500"
+      type="text"
+      id="user-id"
+      placeholder="Your user id"
+    />
   </div>
-  <button type="button" on:click={onClick} class="self-end mt-4 btn btn-primary">
+  <button type="submit" class="self-end mt-4 btn btn-primary" disabled={!canSubmit}>
     Continue
   </button>
 </form>
