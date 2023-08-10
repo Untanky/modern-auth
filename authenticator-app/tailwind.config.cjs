1aa6bc9beb6b3dc69713983baa5bda11be0ffc75
Lukas Grimm <lukaskingsmail@gmail.com>
2023-08-10 21:09:46 +0200

    Install jost as default font

--- authenticator-app/tailwind.config.cjs
+++ authenticator-app/tailwind.config.cjs
@@ -5,6 +5,9 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
+    fontFamily: {
+      sans: ['Jost', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+    },
     extend: {},
   },
 
