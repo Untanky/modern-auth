d322c8ddad394bc62ab083becf20a5d642105189
Lukas Grimm <lukaskingsmail@gmail.com>
2023-08-10 21:43:57 +0200

    Make component for card with icon

---
+++ authenticator-app/src/lib/CardWithIcon.svelte
@@ -0,0 +1,12 @@
+<div class="relative card">
+  <div class="absolute -top-10 left-0 right-0 flex justify-center">
+    <span class="w-20 h-20 p-2 border bg-stone-50 dark:bg-stone-900 dark:border-stone-600 rounded-full">
+    </span>
+  </div>
+  <div class="absolute -top-0 h-10 left-36 right-36 bg-stone-50 dark:bg-stone-900 flex justify-center fill-stone-950 dark:fill-stone-50">
+    <div class="w-20 h-20 p-2 -mt-10">
+      <slot name="icon" />
+    </div>
+  </div>
+  <slot />
+</div>
--- authenticator-app/src/routes/+page.svelte
+++ authenticator-app/src/routes/+page.svelte
@@ -1,15 +1,14 @@
 <script>
+  import CardWithIcon from "$lib/CardWithIcon.svelte";
   import SmartLogin from "$lib/login/SmartLogin.svelte";
   import Identification from "$lib/login/icons/Identification.svelte";
 </script>
 
-<main class="relative sm:w-[420px] card space-y-2">
-  <div class="absolute -top-10 left-0 right-0 flex justify-center">
-    <span class="w-20 h-20 p-2 border bg-stone-50 dark:bg-stone-900 dark:border-stone-600 rounded-full">
-    </span>
-  </div>
-  <div class="absolute top-[-8px] h-10 left-24 right-24 bg-stone-50 dark:bg-stone-900 flex justify-center fill-stone-950 dark:fill-stone-50">
-    <div class="w-20 h-20 p-2 -mt-10"><Identification /></div>
-  </div>
-  <SmartLogin />    
+<main class="sm:w-[420px]">
+  <CardWithIcon>
+    <svelte:fragment slot="icon">
+      <Identification />
+    </svelte:fragment>
+    <SmartLogin />    
+  </CardWithIcon>
 </main>
